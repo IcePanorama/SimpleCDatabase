9bff107b39a7ed327fcb3f3556571310bcb30e34
IcePanorama <thriftedroot@protonmail.com>
2023-12-11 17:52:34 -0500

    created sql compiler and vm

--- src/main.c
+++ src/main.c
@@ -5,6 +5,32 @@
 
 #include "input_buffer.h"
 
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer);
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, 
+        Statement* statement);
+
+void execute_statement(Statement* statement);
+
 int
 main(void)
 {
@@ -15,16 +41,33 @@ main(void)
         print_prompt();
         read_input(input_buffer);
 
-        if(strcmp(input_buffer->buffer, ".exit") == 0)
+        if (input_buffer->buffer[0] == '.')
         {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+            switch (do_meta_command(input_buffer))
+            {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+                    continue;
+            }
         }
-        else
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement))
         {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n", 
+                        input_buffer->buffer);
+                continue;
         }
+
+        execute_statement(&statement);
+        puts("Executed.");
     }
+
 
     return 0;
 }
@@ -50,4 +93,48 @@ read_input(InputBuffer* input_buffer)
     // Ignore trailing newline
     input_buffer->input_length = bytes_read - 1;
     input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+MetaCommandResult 
+do_meta_command(InputBuffer* input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult 
+prepare_statement(InputBuffer* input_buffer, Statement* statement)
+{
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS; 
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void 
+execute_statement(Statement* statement)
+{
+    switch (statement->type)
+    {
+        case (STATEMENT_INSERT):
+            puts("This is where we would do an insert.");
+            break;
+        case (STATEMENT_SELECT):
+            puts("This is where we would do a select.");
+            break;
+    }
 }
