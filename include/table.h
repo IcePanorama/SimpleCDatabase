c02c08b71f909a8a04f91e98e3c8afcee56713aa
IcePanorama <thriftedroot@protonmail.com>
2023-12-18 12:55:13 -0500

    implemented leaf node splitting

--- include/table.h
+++ include/table.h
@@ -71,5 +71,17 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value);
 NodeType get_node_type(void* node);
 void set_node_type(void* node, NodeType type);
 void initialize_leaf_node(void* node);
+void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value);
+uint32_t get_unused_page_num(struct Pager* pager);
+void create_new_root(Table* table, uint32_t right_child_page_num);
+uint32_t* internal_node_num_keys(void* node);
+uint32_t* internal_node_right_child(void* node);
+uint32_t* internal_node_cell(void* node, uint32_t cell_num);
+uint32_t* internal_node_child(void* node, uint32_t child_num);
+uint32_t* internal_node_key(void* node, uint32_t key_num);
+uint32_t get_node_max_key(void* node);
+bool is_node_root(void* node);
+void set_node_root(void* node, bool is_root);
+void initialize_internal_node(void* node);
 
 #endif
--- src/meta_command_result.c
+++ src/meta_command_result.c
@@ -7,14 +7,47 @@
 #include "table.h"
 
 void 
-print_leaf_node(void* node)
+indent(uint32_t level)
 {
-    uint32_t num_cells = *leaf_node_num_cells(node);
-    printf("leaf (size %d)\n", num_cells);
-    for (uint32_t i = 0; i < num_cells; i++)
+    for (uint32_t i = 0; i < level; i++)
     {
-        uint32_t key = *leaf_node_key(node, i);
-        printf("  - %d : %d\n", i, key);
+        printf("  ");
+    }
+}
+
+void 
+print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level)
+{
+    void* node = get_page(pager, page_num);
+    uint32_t num_keys, child;
+
+    switch (get_node_type(node))
+    {
+        case (NODE_LEAF):
+            num_keys = *leaf_node_num_cells(node);
+            indent(indentation_level);
+            printf("- leaf (size %d)\n", num_keys);
+            for (uint32_t i = 0; i < num_keys; i++)
+            {
+                indent(indentation_level + 1);
+                printf("- %d\n", *leaf_node_key(node, i));
+            }
+            break;
+        case (NODE_INTERNAL):
+            num_keys = *internal_node_num_keys(node);
+            indent(indentation_level);
+            printf("- internal (size %d)\n", num_keys);
+            for (uint32_t i = 0; i < num_keys; i++)
+            {
+                child = *internal_node_child(node, i);
+                print_tree(pager, child, indentation_level + 1);
+
+                indent(indentation_level + 1);
+                printf("- key %d\n", *internal_node_key(node, i));
+            }
+        child = *internal_node_right_child(node);
+        print_tree(pager, child, indentation_level + 1);
+        break;
     }
 }
 
@@ -40,7 +73,7 @@ do_meta_command(InputBuffer* input_buffer, Table* table)
     else if (strcmp(input_buffer->buffer, ".btree") == 0)
     {
         puts("Tree:");
-        print_leaf_node(get_page(table->pager, 0));
+        print_tree(table->pager, 0, 0);
         return META_COMMAND_SUCCESS;
     }
     else if (strcmp(input_buffer->buffer, ".constants") == 0)
