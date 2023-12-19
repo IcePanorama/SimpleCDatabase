94c1bd758ed8ab519a8499d619e0f418371148dc
IcePanorama <thriftedroot@protonmail.com>
2023-12-19 11:03:39 -0500

    implemented recursively searching the b-tree

--- src/cursor.c
+++ src/cursor.c
@@ -6,6 +6,8 @@
 #include "cursor.h"
 #include "pager.h"
 
+Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key);
+
 Cursor* 
 table_start(Table* table)
 {
@@ -33,11 +35,11 @@ table_find(Table* table, uint32_t key)
     }
     else
     {
-        puts("Need to implement searching an internal node.");
-        exit(EXIT_FAILURE);
+        return internal_node_find(table, root_page_num, key);
     }
 }
 
+// Should this be in table.c?
 Cursor* 
 leaf_node_find(Table* table, uint32_t page_num, uint32_t key)
 {
@@ -94,5 +96,40 @@ cursor_advance(Cursor* cursor)
     if (cursor->cell_num >= (*leaf_node_num_cells(node)))
     {
         cursor->end_of_table = true;
+    }
+}
+
+Cursor* 
+internal_node_find(Table* table, uint32_t page_num, uint32_t key)
+{
+    void* node = get_page(table->pager, page_num);
+    uint32_t num_keys = *internal_node_num_keys(node);
+
+    /* Binary search to find the index of child to search */
+    uint32_t min_index = 0;
+    uint32_t max_index = num_keys;  /* there is one more child than keys */
+
+    while (min_index != max_index)
+    {
+        uint32_t index = (min_index + max_index) / 2;
+        uint32_t key_to_right = *internal_node_key(node, index);
+        if (key_to_right >= key)
+        {
+            max_index = index;
+        }
+        else
+        {
+            min_index = index + 1;
+        }
+    }
+
+    uint32_t child_num = *internal_node_child(node, min_index);
+    void* child = get_page(table->pager, child_num);
+    switch (get_node_type(child))
+    {
+        case NODE_LEAF:
+            return leaf_node_find(table, child_num, key);
+        case NODE_INTERNAL:
+            return internal_node_find(table, child_num, key);
     }
 }
