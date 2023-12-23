9bc9ebfa96f8a079b0643b31fac57ab2176b7400
IcePanorama <thriftedroot@protonmail.com>
2023-12-23 17:06:35 -0500

    Now splitting internal nodes but it has a few bugs

--- src/meta_command_result.c
+++ src/meta_command_result.c
@@ -37,16 +37,19 @@ print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level)
             num_keys = *internal_node_num_keys(node);
             indent(indentation_level);
             printf("- internal (size %d)\n", num_keys);
-            for (uint32_t i = 0; i < num_keys; i++)
+            if (num_keys > 0)
             {
-                child = *internal_node_child(node, i);
-                print_tree(pager, child, indentation_level + 1);
+                for (uint32_t i = 0; i < num_keys; i++)
+                {
+                    child = *internal_node_child(node, i);
+                    print_tree(pager, child, indentation_level + 1);
 
-                indent(indentation_level + 1);
-                printf("- key %d\n", *internal_node_key(node, i));
+                    indent(indentation_level + 1);
+                    printf("- key %d\n", *internal_node_key(node, i));
+                }
+                child = *internal_node_right_child(node);
+                print_tree(pager, child, indentation_level + 1);
             }
-        child = *internal_node_right_child(node);
-        print_tree(pager, child, indentation_level + 1);
         break;
     }
 }
